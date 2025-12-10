28ebc98cd794a60305104aab703a32fbca2ea16d
Rotimi Collins <75532154+amethystcoder@users.noreply.github.com>
2025-12-10 11:06:51 +0000

    Update join button styles for improved interaction and visual consistency

--- programs/programsDisplay.js
+++ programs/programsDisplay.js
@@ -70,7 +70,10 @@ function incrementProgramDisplay() {
     document.getElementById('program-image').style.backgroundImage = `url(${program.imageUrl})`;
     document.getElementById('program-image').style.backgroundColor = program.forgroundColor;
     document.getElementById('program-card').style.backgroundColor = program.themeColor;
-    document.getElementById('join-button').style.backgroundColor = program.othercolor;
+    document.getElementById('join-button').style.backgroundColor = program.themeColor;
+    document.getElementById('join-button').onmouseover((ev)=>{
+        ev.target.style.color = program.themeColor;
+    }) = program.othercolor;
     //sliding animation
     const programCard = document.getElementById('program-card');
     programCard.classList.remove('slide-in');
